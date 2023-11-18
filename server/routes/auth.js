df05475e46d25f17cc00ab48560f2969075c13f4
LuckyCharm2023 <151199137+LuckyCharm2023@users.noreply.github.com>
2023-11-18 22:23:19 +0530

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { registerUser } = require("../controllers/auth");
-const router = express.Router();
-
-router.post("/register", registerUser);
-module.exports = router;
