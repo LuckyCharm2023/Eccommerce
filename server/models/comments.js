7095d77fea32920446a6697e9dfbc45ec14555a4
LuckyCharm2023 <151199137+LuckyCharm2023@users.noreply.github.com>
2023-11-21 09:14:41 +0530

    Delete server directory

--- server/models/comments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const commentSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  productId: { type: String, required: true },
-  comment: { type: String, required: true },
-  rating: { type: String, required: true },
-});
-const Comment = mongoose.model("Comment", commentSchema);
-exports.Comment = Comment;
--- server/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createComment, getComment } = require("../controllers/comments");
-const router = express.Router();
-
-router.post("/createComment", createComment);
-router.get("/getComments/:id", getComment);
-
-module.exports = router;
