fb32ba7f4fe20d7802e4987e9f3895af153d2605
LuckyCharm2023 <151199137+LuckyCharm2023@users.noreply.github.com>
2023-11-23 08:21:58 +0530

    Update comments.js

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/createComment", createComment);
 router.get("/getComments/:id", getComment);
+router.get("/getComments", getAllComment);
 router.put("/updateComment/:id", updateComment);
 router.delete("/deleteComment/:id", deleteComment);
 
